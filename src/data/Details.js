dc0e3e734bd0eb987e44d4b1d060daa5744970a1
haneeshbyreddy <haneeshbyreddy@gmail.com>
2024-02-08 12:50:14 +0000

    start

---
+++ src/data/Details.js
@@ -0,0 +1,241 @@
+export const DETAILS = {
+  name: "Haneesh Reddy",
+  logo: "assets/images/name.svg",
+  location: "Hyderabad, Telangana, India",
+  resumeLink:
+    "https://drive.google.com/drive/folders/1-8VACahW5TyFVZPTp5F-77Nd7BJyqVOV",
+  profile_links: {
+    email: {
+      link: "haneeshbyreddy@gmail.com",
+      image: "assets/images/outlook.svg",
+    },
+    github: {
+      link: "https://github.com/haneesh-25",
+      image: "assets/images/github.svg",
+    },
+    linkedin: {
+      link: "https://www.linkedin.com/in/haneeshbyreddy/",
+      image: "assets/images/linkedin.svg",
+    },
+  },
+  profilephoto: "assets/images/profilephoto.jpg",
+  role: ["Software Developer", 2000, "MERN Stack Developer", 2000],
+  tag_line:
+    "Knack of building applications with front and back end operations.",
+  about_me: {
+    para1: "<HelloViewer/> Warm welcome, nice to see you here",
+    para2:
+      "Highly motivated and results-oriented final year Bachelor of Engineering student seeking an entry-level position to leverage strong analytical, problem-solving, and communication skills.",
+    para3:
+      "Possess a solid foundation in DSA and a keen interest in applying theoretical knowledge to real-world challenges. ",
+    para4:
+      "Eager to contribute to a dynamic team and learn from experienced professionals.",
+  },
+  education: {
+    college: {
+      name: "ITM University, Gwalior",
+      course: "B.Tech Computer Science",
+      grade: "80%",
+      timeline: "2020-2024",
+      image: "assets/images/college.jpg",
+    },
+    higher_secondary_school: {
+      name: "Narayana IIT Academy - Raviryala",
+      course: "12th Standard",
+      grade: "91 %",
+      timeline: "2018-2020",
+      image: "assets/images/school.jpg",
+    },
+    secondary_school: {
+      name: "Indian Matriculation Higher Secondary School, Harur",
+      course: "10th Standard",
+      grade: "88.64 %",
+      timeline: "2003-2002",
+      image: "assets/images/school2.jpeg",
+    },
+  },
+  certifications: [
+    {
+      certid: "1",
+      coursename: "Front End Devlopment Libraries",
+      userid: "H3586umB01232zQ645",
+      verifyurl:
+        "https://freecodecamp.org/certification/haneeshbyreddy/front-end-development-libraries",
+      image: "assets/images/frontendlibraries.png",
+      certifiedby: "freeCodeCamp",
+      timeline: "October 2023",
+    },
+    {
+      certid: "2",
+      coursename: "JavaScript Data Structure and Algorithm",
+      userid: "167931Y392K35sMb87",
+      verifyurl:
+        "https://www.freecodecamp.org/certification/haneeshbyreddy/javascript-algorithms-and-data-structures",
+      image: "assets/images/javascriptdatastruture.png",
+      certifiedby: "freeCodeCamp",
+      timeline: "September 2023",
+    },
+    {
+      certid: '3',
+      coursename: "Responsive Web Design",
+      userid: "something",
+      verifyurl: 'https://freecodecamp.org/certification/haneeshbyreddy/responsive-web-design',
+      image: 'assets/images/responsivewebdesign.png',
+      certifiedby: 'freeCodeCamp',
+      timeline: "September 2023"
+    }
+  ],
+  skills: [
+    {
+      skillid: "1",
+      image: "assets/images/python.svg",
+      name: "PYTHON",
+      rating: "⭐⭐⭐⭐",
+    },
+    {
+      skillid: "2",
+      image: "assets/images/c++.svg",
+      name: "C++",
+      rating: "⭐⭐⭐",
+    },
+    {
+      skillid: "3",
+      image: "assets/images/java.svg",
+      name: "JAVA",
+      rating: "⭐⭐⭐⭐",
+    },
+    {
+      skillid: "4",
+      image: "assets/images/javascript.svg",
+      name: "JavaScript",
+      rating: "⭐⭐⭐⭐",
+    },
+    {
+      skillid: "5",
+      image: "assets/images/react.svg",
+      name: "ReactJS",
+      rating: "⭐⭐⭐",
+    },
+    {
+      skillid: "6",
+      image: "assets/images/nodejs.svg",
+      name: "NodeJS",
+      rating: "⭐⭐⭐⭐",
+    },
+    {
+      skillid: "7",
+      image: "assets/images/expressjs.svg",
+      name: "ExpressJS",
+      rating: "⭐⭐⭐⭐",
+    },
+    {
+      skillid: "8",
+      image: "assets/images/mongodb.svg",
+      name: "MongoDB",
+      rating: "⭐⭐⭐",
+    },
+    {
+      skillid: "9",
+      image: "assets/images/git.svg",
+      name: "Git",
+      rating: "⭐⭐⭐⭐",
+    },
+  ],
+  projects: [
+    {
+      id: "1",
+      title: "Money Manager",
+      period: "Sep 2021 - Oct 2021",
+      subtitle: "Expense Tracker Application",
+      link: "https://sridharrajaram-moneymanager-client.netlify.app/",
+      FE_ghlink: "https://github.com/sridharrajaram/moneymanager_client.git",
+      BE_ghlink: "https://github.com/sridharrajaram/moneymanager_server.git",
+      image: "assets/images/moneymanager.png",
+      description: {
+        para1: "The application help us to maintain our daily expenses.",
+        para2:
+          "The application is made completely user-friendly and has an amazing user-experience",
+      },
+    },
+  ],
+  experiences: [
+    {
+      id: 1,
+      company: "Vodafone Idea Limited",
+      positions: [
+        {
+          id: 1,
+          positions: "Assistant Manager",
+          type: "Full Time",
+          description:
+            "Worked as EMF Regulatory Compliance Manager and governing its IT websites",
+          timeline: "June 2014 - Oct 2020",
+        },
+      ],
+    },
+    {
+      id: 2,
+      company: "Gauri Telecommunications",
+      positions: [
+        {
+          id: 1,
+          positions: "Senior Engineer",
+          type: "Full-time",
+          description:
+            "Worked as a Consultant in Ericsson India Private Ltd, Karnataka",
+          timeline: "April 2014 - May 2014",
+        },
+      ],
+    },
+    {
+      id: 3,
+      company: "Haveus Solutions Private Limited",
+      positions: [
+        {
+          id: 1,
+          positions: "Solution Service Level I",
+          type: "Full-time",
+          description:
+            "Worked as a Consultant in Ericsson India Private Ltd, Karnataka",
+          timeline: "May 2012 - March 2014",
+        },
+      ],
+    },
+    {
+      id: 4,
+      company: "Telecom Network Solutions Private Limited",
+      positions: [
+        {
+          id: 1,
+          positions: "RF Engineer",
+          type: "Full-time",
+          description:
+            "Worked as a Consultant in Ericsson India Private Ltd, Karnataka",
+          timeline: "May 2009 - May 2012",
+        },
+      ],
+    },
+  ],
+  achievements: [
+    {
+      id: 1,
+      star: "one",
+    },
+    {
+      id: 2,
+      star: "two",
+    },
+    {
+      id: 3,
+      star: "three",
+    },
+    {
+      id: 4,
+      star: "four",
+    },
+    {
+      id: 5,
+      star: "five",
+    },
+  ],
+};
