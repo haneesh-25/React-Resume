2083d829fcaadaf5327e82e08610a9b012441cb5
haneeshbyreddy <haneeshbyreddy@gmail.com>
2024-02-09 10:05:16 +0000

    skills grid view changed

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -13,7 +13,7 @@ function Skills(props) {
             return (
               <div
                 key={skill.skillid}
-                className="col-12 col-md-3 skill-tile text-center"
+                className="col-4 col-md-2 skill-tile text-center"
               >
                 <img src={skill.image} alt="Skill" width="30%" height="20%" />
                 <p>{skill.name}</p>
